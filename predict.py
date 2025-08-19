ae52dda36330f2fde499da7e814568f8c9bc13f1
ThomasZadikian <110538223+ThomasZadikian@users.noreply.github.com>
2025-08-19 11:26:22 +0200

    Add logs.

--- predict.py
+++ predict.py
@@ -1,15 +1,21 @@
 import torch
+import logging
 from transformers import pipeline
 
 model_path = "emotion_classifier_results/checkpoint-1875"
 
 print(f"Chargement du modèle depuis : {model_path}")
+logging.basicConfig(level=logging.DEBUG,
+        filename="logs/conversation.log",
+        filemode="a",
+        format='%(asctime)s - %(levelname)s - %(message)s')
 
 emotion_classifier = pipeline(
     "text-classification",
     model=model_path,
     top_k=None
 )
+emotion = ""
 
 def predict_emotions(text, threshold=0.3):
     """
@@ -23,6 +29,7 @@ def predict_emotions(text, threshold=0.3):
     
     all_emotions_sorted = sorted(results, key=lambda x: x['score'], reverse=True)
     top_3_emotions = [(e['label'], f"{e['score']:.2%}") for e in all_emotions_sorted[:3]]
+    emotions = top_3_emotions
     print(f"Top 3 des prédictions (scores bruts) : {top_3_emotions}")
 
     detected_emotions = []
@@ -33,9 +40,9 @@ def predict_emotions(text, threshold=0.3):
             
     detected_emotions.sort(key=lambda x: x[1], reverse=True)
     
+    logging.info(f"La phrase est la suivante : {text} et les emotions detectees sont : {detected_emotions}")
     return detected_emotions
 
-# Test of prediction function
 sentence_example = [
     "I am so happy that I get to work on this exciting project!",
     "This is really frustrating, the code is not working at all.",
@@ -46,11 +53,9 @@ for sentence in sentence_example:
     print("\n--- Fin de la prédiction des émotions ---\n")
 
 
-# Execution of prediction for user input
 while True:
     sentence = input("Entrez une phrase pour détecter les émotions (ou 'exit' pour quitter) : ")
     if sentence.lower() == 'exit':
         break
     else:
-        open('/logs/conversation_log.txt', 'a').write(f"Phrase: {sentence}\nÉmotions détectées: {emotions}\n\n")
         emotions = predict_emotions(sentence)
